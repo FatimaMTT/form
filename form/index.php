cb2832e570c89296d76d7035c9591c06d84f769e
Fatima Malo Torres Trueba <fatamtt@gmail.com>
2018-03-11 15:11:35 -0400

    Add files via upload

--- form/index.php
+++ form/index.php
@@ -6,7 +6,7 @@
     <title>Form</title>
     <meta name="description" content="Form">
     <meta name="author" content="Fatima Malo Torres Trueba">
-	<link rel="stylesheet" type="text/css" href="style/global.css" />
+	<link rel="stylesheet" type="text/css" href="style/global.css?random.number" />
 </head>
 
 <body>
@@ -54,15 +54,15 @@
     }
     ?>
     
-    <form method="post">
-        <h1>Sign up to Fatima's form:</h1>
-        <p>Username</p><input type="text" name="username">
-        <p>Password</p><input type="password" name="password">
-        <p>First Name</p><input type="text" name="firstname">
-        <p>Last Name</p><input type="text" name="lastname">
-        <p>Email</p><input type="text" name="email">
+    <form id="grid-container" method="post">
+        <h1 id="item1">Sign up to Fatima's form:</h1>
+        <p id="item2">Username</p><input id="item3" type="text" name="username">
+        <p id="item4">Password</p><input id="item5" type="password" name="password">
+        <p id="item6">First Name</p><input id="item7" type="text" name="firstname">
+        <p id="item8">Last Name</p><input id="item9" type="text" name="lastname">
+        <p id="item10">Email</p><input id="item11" type="text" name="email">
         <input id="signupbutton" type="submit" name="signup" value="Sign Up">        
-</form> 
+    </form> 
     
 </body>
 </html>
